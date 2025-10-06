2656e81cd3e4163caf888f66a0e306da7405dabc
JoseAngel <angel.gonzalez.rosas@gmail.com>
2025-10-06 17:48:27 -0600

    Initial commit: Estructura base del proyecto FinanSmart

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
-export const environment = {
-  production: true
+﻿export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'TU_API_KEY_PROD',
+    authDomain: 'TU_AUTH_DOMAIN_PROD',
+    projectId: 'TU_PROJECT_ID_PROD',
+    storageBucket: 'TU_STORAGE_BUCKET_PROD',
+    messagingSenderId: 'TU_MESSAGING_SENDER_ID_PROD',
+    appId: 'TU_APP_ID_PROD'
+  },
+  exchangeRateApiKey: 'TU_EXCHANGE_RATE_API_KEY_PROD'
 };
