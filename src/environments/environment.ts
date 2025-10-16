5d51c9833c9032c08ec3365f663eea1a1b5f773f
J0s34n <90327700+J0s34n@users.noreply.github.com>
2025-10-16 17:50:09 -0600

    Update exchangeRateApiKey in environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,6 @@
     messagingSenderId: "937351972055",
     appId: "1:937351972055:web:c9c8f1f6e9a39edde81c92"
   },
-  exchangeRateApiKey: 'TU_EXCHANGE_RATE_API_KEY',
+  exchangeRateApiKey: '47f642034969413ebf03e8e15f4907e1',
   imgbbApiKey: '6feb3abc60f237bac823a47a68be8c43'
 };
