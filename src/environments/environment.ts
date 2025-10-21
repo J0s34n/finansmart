b3f3ea2ffb9ef2d0cba79366f420e15ec65778ed
J0s34n <90327700+J0s34n@users.noreply.github.com>
2025-10-21 00:43:48 -0600

    Remove sensitive API keys from environment.ts
    Removed sensitive API keys from environment configuration for security reasons.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,2 @@
-﻿export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyAexmuC4uCsavx1JtUaZYt9v7v-EVDu2r0",
-    authDomain: "finansmart-93ea9.firebaseapp.com",
-    projectId: "finansmart-93ea9",
-    storageBucket: "finansmart-93ea9.firebasestorage.app",
-    messagingSenderId: "937351972055",
-    appId: "1:937351972055:web:c9c8f1f6e9a39edde81c92"
-  },
-  exchangeRateApiKey: '47f642034969413ebf03e8e15f4907e1',
-  imgbbApiKey: '6feb3abc60f237bac823a47a68be8c43'
-};
+// por seguridad no se permite ver
+
